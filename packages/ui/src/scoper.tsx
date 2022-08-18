647c78599ab80b3fef7ff33dbfbf3a0ac5718b0a
Esa-Matti Suuronen <esa-matti@suuronen.org>
2022-08-18 18:07:55 +0300

    prettier

--- packages/ui/src/scoper.tsx
+++ packages/ui/src/scoper.tsx
@@ -2,111 +2,99 @@ import React from "react";
 import { ElementType, forwardRef } from "react";
 
 export type ScopeCSS<
-    Prefix extends string,
-    Values extends string,
+	Prefix extends string,
+	Values extends string
 > = Values extends `${Prefix}--${infer rest}` ? rest : never;
 
 export interface ScopedStyle {
-    __scoped: string;
+	__scoped: string;
 }
 
 /**
  * See __tests__/scoper.test.tsx
  */
 export function createClassNameScoper<KnownStyles extends string>() {
-    function scopeClassNames<Prefix extends string>(scope: Prefix) {
-        type ScopedClassNames = ScopeCSS<Prefix, KnownStyles>;
-        type ScopedStyleObject = Partial<
-            Record<ScopedClassNames, boolean | undefined | null>
-        >;
+	function scopeClassNames<Prefix extends string>(scope: Prefix) {
+		type ScopedClassNames = ScopeCSS<Prefix, KnownStyles>;
+		type ScopedStyleObject = Partial<
+			Record<ScopedClassNames, boolean | undefined | null>
+		>;
 
-        type Args<T> = ScopedClassNames extends never
-            ? never
-            :
-                  | T
-                  | ScopedClassNames
-                  | ScopedStyleObject
-                  | null
-                  | false
-                  | undefined;
+		type Args<T> = ScopedClassNames extends never
+			? never
+			: T | ScopedClassNames | ScopedStyleObject | null | false | undefined;
 
-        function classNames(...names: Args<ScopedStyle>[]): string {
-            return names
-                .flatMap((n) => {
-                    if (!n) {
-                        return [];
-                    }
+		function classNames(...names: Args<ScopedStyle>[]): string {
+			return names
+				.flatMap((n) => {
+					if (!n) {
+						return [];
+					}
 
-                    if (typeof n === "string") {
-                        return [`${scope}--${n}`];
-                    }
+					if (typeof n === "string") {
+						return [`${scope}--${n}`];
+					}
 
-                    if ("__scoped" in n) {
-                        return [n.__scoped];
-                    }
+					if ("__scoped" in n) {
+						return [n.__scoped];
+					}
 
-                    return Object.entries(n).flatMap(([className, inUse]) => {
-                        if (inUse) {
-                            return classNames(className as any);
-                        }
+					return Object.entries(n).flatMap(([className, inUse]) => {
+						if (inUse) {
+							return classNames(className as any);
+						}
 
-                        return [];
-                    });
-                })
-                .join(" ");
-        }
+						return [];
+					});
+				})
+				.join(" ");
+		}
 
-        classNames.nest = (...names: Args<null>[]): ScopedStyle => {
-            return {
-                __scoped: classNames(...names),
-            };
-        };
+		classNames.nest = (...names: Args<null>[]): ScopedStyle => {
+			return {
+				__scoped: classNames(...names),
+			};
+		};
 
-        Object.assign(classNames, { cn: classNames });
+		Object.assign(classNames, { cn: classNames });
 
-        return classNames;
-    }
+		return classNames;
+	}
 
-    function scopeView<Prefix extends string>(prefix: Prefix) {
-        const scopeCSS = scopeClassNames(prefix);
+	function scopeView<Prefix extends string>(prefix: Prefix) {
+		const scopeCSS = scopeClassNames(prefix);
 
-        type ViewProps<T> = {
-            as?: T;
-            cn?: Parameters<typeof scopeCSS> | Parameters<typeof scopeCSS>[0];
-            ref?: React.Ref<
-                T extends keyof HTMLElementTagNameMap
-                    ? HTMLElementTagNameMap[T]
-                    : T
-            >;
-            children?: React.ReactNode;
-        };
+		type ViewProps<T> = {
+			as?: T;
+			cn?: Parameters<typeof scopeCSS> | Parameters<typeof scopeCSS>[0];
+			ref?: React.Ref<
+				T extends keyof HTMLElementTagNameMap ? HTMLElementTagNameMap[T] : T
+			>;
+			children?: React.ReactNode;
+		};
 
-        function ViewType<T extends ElementType = "div">(
-            _props: React.ComponentPropsWithoutRef<T> & ViewProps<T>,
-        ): JSX.Element {
-            return <></>;
-        }
+		function ViewType<T extends ElementType = "div">(
+			_props: React.ComponentPropsWithoutRef<T> & ViewProps<T>
+		): JSX.Element {
+			return <></>;
+		}
 
-        const View: typeof ViewType = forwardRef(
-            ({ as, cn, ...props }: any, ref) => {
-                const Component = as || "div";
-                const cnArgs = Array.isArray(cn) ? cn : [cn];
+		const View: typeof ViewType = forwardRef(
+			({ as, cn, ...props }: any, ref) => {
+				const Component = as || "div";
+				const cnArgs = Array.isArray(cn) ? cn : [cn];
 
-                const classes = [
-                    "view",
-                    props.className,
-                    cn ? scopeCSS(...cnArgs) : null,
-                ]
-                    .filter(Boolean)
-                    .join(" ");
+				const classes = [props.className, cn ? scopeCSS(...cnArgs) : null]
+					.filter(Boolean)
+					.join(" ");
 
-                return <Component {...props} className={classes} ref={ref} />;
-            },
-        ) as any;
+				return <Component {...props} className={classes} ref={ref} />;
+			}
+		) as any;
 
-        Object.assign(View, { displayName: `View[${prefix}]` });
-        return View;
-    }
+		Object.assign(View, { displayName: `View[${prefix}]` });
+		return View;
+	}
 
-    return { scopeClassNames, scopeView };
+	return { scopeClassNames, scopeView };
 }
