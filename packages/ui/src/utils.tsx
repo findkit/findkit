9e8bf6313f8ec29a4f865f3d9fa6d43db1f40352
Esa-Matti Suuronen <esa-matti@suuronen.org>
2022-08-18 16:51:00 +0300

    prettier

--- packages/ui/src/utils.tsx
+++ packages/ui/src/utils.tsx
@@ -5,24 +5,24 @@ import { createClassNameScoper } from "./scoper";
  * https://stackoverflow.com/a/25456134/153718
  */
 export function isDeepEqual(x: any, y: any) {
-    if (x === y) {
-        return true;
-    } else if (
-        typeof x == "object" &&
-        x != null &&
-        typeof y == "object" &&
-        y != null
-    ) {
-        if (Object.keys(x).length !== Object.keys(y).length) return false;
+	if (x === y) {
+		return true;
+	} else if (
+		typeof x == "object" &&
+		x != null &&
+		typeof y == "object" &&
+		y != null
+	) {
+		if (Object.keys(x).length !== Object.keys(y).length) return false;
 
-        for (const prop in x) {
-            if (y.hasOwnProperty(prop)) {
-                if (!isDeepEqual(x[prop], y[prop])) return false;
-            } else return false;
-        }
+		for (const prop in x) {
+			if (y.hasOwnProperty(prop)) {
+				if (!isDeepEqual(x[prop], y[prop])) return false;
+			} else return false;
+		}
 
-        return true;
-    } else return false;
+		return true;
+	} else return false;
 }
 
 /**
@@ -30,30 +30,30 @@ export function isDeepEqual(x: any, y: any) {
  * tests
  */
 export function cleanUndefined<T extends {}>(ob: T): T {
-    const out = {} as T;
+	const out = {} as T;
 
-    for (const key in ob) {
-        if (ob[key] !== undefined) {
-            out[key] = ob[key];
-        }
-    }
+	for (const key in ob) {
+		if (ob[key] !== undefined) {
+			out[key] = ob[key];
+		}
+	}
 
-    return out;
+	return out;
 }
 
 /**
  * Asserts that given object is not null or undefined
  */
 export function assertNonNullable<T>(
-    ob: T,
-    assertionMessage: string,
+	ob: T,
+	assertionMessage: string
 ): asserts ob is NonNullable<T> {
-    if (ob === null || ob === undefined) {
-        throw new Error(assertionMessage);
-    }
+	if (ob === null || ob === undefined) {
+		throw new Error(assertionMessage);
+	}
 }
 
 export const { scopeClassNames, scopeView } =
-    createClassNameScoper<ClassNames>();
+	createClassNameScoper<ClassNames>();
 
 export const View = scopeView("findkit");
