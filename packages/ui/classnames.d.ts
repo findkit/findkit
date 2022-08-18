99590d0160128740fddda61c8d4fd7b77a9b000d
Esa-Matti Suuronen <esa-matti@suuronen.org>
2022-08-18 19:24:31 +0300

    Hide header when scrolling down

--- packages/ui/classnames.d.ts
+++ packages/ui/classnames.d.ts
@@ -3,9 +3,11 @@
 type ClassNames =
   | "findkit--back-link"
   | "findkit--close-button"
+  | "findkit--content"
   | "findkit--cross"
   | "findkit--group-title"
   | "findkit--header"
+  | "findkit--header-hidden"
   | "findkit--hit"
   | "findkit--load-more-button"
   | "findkit--loading"
