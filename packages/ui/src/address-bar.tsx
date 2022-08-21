1000623b307c9c0f81a57b1adaaaa87c19bce7d7
Esa-Matti Suuronen <esa-matti@suuronen.org>
2022-08-21 14:37:46 +0300

    AddressBar prettier

--- packages/ui/src/address-bar.tsx
+++ packages/ui/src/address-bar.tsx
@@ -2,134 +2,134 @@
  * @public
  */
 export interface AddressBar {
-    listen(cb: () => any): () => void;
-    getSearchParamsString(): string;
-    update: (params: URLSearchParams, options?: { push?: boolean }) => void;
+	listen(cb: () => any): () => void;
+	getSearchParamsString(): string;
+	update: (params: URLSearchParams, options?: { push?: boolean }) => void;
 }
 
 const CUSTOM_EVENT_NAME = "findkit-change";
 
 function dispatchFindkitHistoryEvent() {
-    dispatchEvent(new CustomEvent(CUSTOM_EVENT_NAME));
+	dispatchEvent(new CustomEvent(CUSTOM_EVENT_NAME));
 }
 
 /**
  * @public
  */
 export class FindkitURLSearchParams {
-    private params: URLSearchParams;
-    private instanceId: string;
+	private params: URLSearchParams;
+	private instanceId: string;
 
-    constructor(instanceId: string, search: string) {
-        this.instanceId = instanceId;
-        this.params = new URLSearchParams(search);
-    }
+	constructor(instanceId: string, search: string) {
+		this.instanceId = instanceId;
+		this.params = new URLSearchParams(search);
+	}
 
-    getGroupId() {
-        return this.params.get(this.instanceId + "_id")?.trim() || undefined;
-    }
+	getGroupId() {
+		return this.params.get(this.instanceId + "_id")?.trim() || undefined;
+	}
 
-    next(fn: (params: FindkitURLSearchParams) => void) {
-        const next = new FindkitURLSearchParams(
-            this.instanceId,
-            this.params.toString(),
-        );
-        fn(next);
-        return next;
-    }
+	next(fn: (params: FindkitURLSearchParams) => void) {
+		const next = new FindkitURLSearchParams(
+			this.instanceId,
+			this.params.toString()
+		);
+		fn(next);
+		return next;
+	}
 
-    clearGroupId() {
-        return this.next((next) => {
-            next.params.delete(next.instanceId + "_id");
-        });
-    }
+	clearGroupId() {
+		return this.next((next) => {
+			next.params.delete(next.instanceId + "_id");
+		});
+	}
 
-    clearAll() {
-        return this.next((next) => {
-            next.params.delete(next.instanceId + "_id");
-            next.params.delete(next.instanceId + "_q");
-        });
-    }
+	clearAll() {
+		return this.next((next) => {
+			next.params.delete(next.instanceId + "_id");
+			next.params.delete(next.instanceId + "_q");
+		});
+	}
 
-    setGroupId(id: string) {
-        return this.next((next) => {
-            next.params.set(next.instanceId + "_id", id);
-        });
-    }
+	setGroupId(id: string) {
+		return this.next((next) => {
+			next.params.set(next.instanceId + "_id", id);
+		});
+	}
 
-    setTerms(terms: string) {
-        return this.next((next) => {
-            next.params.set(next.instanceId + "_q", terms.trim());
-        });
-    }
+	setTerms(terms: string) {
+		return this.next((next) => {
+			next.params.set(next.instanceId + "_q", terms.trim());
+		});
+	}
 
-    isActive() {
-        return this.params.has(this.instanceId + "_q");
-    }
+	isActive() {
+		return this.params.has(this.instanceId + "_q");
+	}
 
-    getTerms() {
-        return (this.params.get(this.instanceId + "_q") || "").trim();
-    }
+	getTerms() {
+		return (this.params.get(this.instanceId + "_q") || "").trim();
+	}
 
-    toString() {
-        return this.params.toString();
-    }
+	toString() {
+		return this.params.toString();
+	}
 
-    toLink() {
-        return "?" + this.toString();
-    }
+	toLink() {
+		return "?" + this.toString();
+	}
 
-    toURLSearchParams() {
-        return this.params;
-    }
+	toURLSearchParams() {
+		return this.params;
+	}
 }
 
 export function createAddressBar(): AddressBar {
-    if (typeof window === "undefined") {
-        return {
-            listen: () => () => {},
-            getSearchParamsString: () => "",
-            update: () => {},
-        };
-    }
+	if (typeof window === "undefined") {
+		return {
+			listen: () => () => {},
+			getSearchParamsString: () => "",
+			update: () => {},
+		};
+	}
 
-    return {
-        getSearchParamsString: () => location.search,
-        update: (params, options) => {
-            const args = [
-                undefined,
-                "",
-                "?" + params.toString() + location.hash,
-            ] as const;
+	return {
+		getSearchParamsString: () => location.search,
+		update: (params, options) => {
+			const args = [
+				undefined,
+				"",
+				"?" + params.toString() + location.hash,
+			] as const;
 
-            if (options?.push) {
-                history.pushState(...args);
-            } else {
-                history.replaceState(...args);
-            }
-            dispatchFindkitHistoryEvent();
-        },
-        listen: (cb: Parameters<AddressBar["listen"]>[0]) => {
-            if (typeof window === "undefined") {
-                return () => {};
-            }
+			if (options?.push) {
+				history.pushState(...args);
+			} else {
+				history.replaceState(...args);
+			}
+			dispatchFindkitHistoryEvent();
+		},
+		listen: (cb: Parameters<AddressBar["listen"]>[0]) => {
+			if (typeof window === "undefined") {
+				return () => {};
+			}
 
-            const onSearchChange = () => {
-                cb();
-            };
+			const onSearchChange = () => {
+				cb();
+			};
 
-            const onPopState = () => {
-                dispatchFindkitHistoryEvent();
-            };
+			const onPopState = () => {
+				dispatchFindkitHistoryEvent();
+			};
 
-            // Convert browser back/forward button presses to Valu Search History events
-            window.addEventListener("popstate", onPopState);
-            window.addEventListener(CUSTOM_EVENT_NAME, onSearchChange);
+			// Convert browser back/forward button presses to Valu Search History events
+			window.addEventListener("popstate", onPopState);
+			window.addEventListener(CUSTOM_EVENT_NAME, onSearchChange);
 
-            return () => {
-                window.removeEventListener("popstate", onPopState);
-                window.removeEventListener(CUSTOM_EVENT_NAME, onSearchChange);
-            };
-        },
-    };
+			return () => {
+				window.removeEventListener("popstate", onPopState);
+				window.removeEventListener(CUSTOM_EVENT_NAME, onSearchChange);
+			};
+		},
+	};
 }
