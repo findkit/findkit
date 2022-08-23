1f18c736866e580dacdadf9f8057f0433cdf5972
Esa-Matti Suuronen <esa-matti@suuronen.org>
2022-08-23 16:17:43 +0300

    Add ui-strings tests

---
+++ packages/ui/tests/ui-strings.spec.ts
@@ -0,0 +1,104 @@
+import { expect, test } from "@playwright/test";
+import type { FindkitUI } from "../src/cdn-entries/index";
+
+declare const MOD: typeof import("../src/cdn-entries/index");
+declare const ui: FindkitUI;
+
+test("can set ui language", async ({ page }) => {
+	await page.goto("/dummy");
+
+	const closeButton = page.locator(".findkit--close-button");
+
+	await page.evaluate(async () => {
+		const ui = new MOD.FindkitUI({
+			publicToken: "po8GK3G0r",
+			ui: { lang: "fi" },
+			params: {
+				tagQuery: [],
+			},
+		});
+
+		await ui.open();
+	});
+
+	await expect(closeButton).toHaveText("Sulje");
+});
+
+test("can change ui language on the fly", async ({ page }) => {
+	await page.goto("/dummy");
+
+	const closeButton = page.locator(".findkit--close-button");
+
+	await page.evaluate(async () => {
+		const ui = new MOD.FindkitUI({
+			publicToken: "po8GK3G0r",
+			params: {
+				tagQuery: [],
+			},
+		});
+
+		await ui.open();
+		Object.assign(window, { ui });
+	});
+
+	await expect(closeButton).toHaveText("Close");
+
+	await page.evaluate(async () => {
+		ui.setUIStrings("fi");
+	});
+
+	await expect(closeButton).toHaveText("Sulje");
+});
+
+test("can customize ui language", async ({ page }) => {
+	await page.goto("/dummy");
+
+	const closeButton = page.locator(".findkit--close-button");
+
+	await page.evaluate(async () => {
+		const ui = new MOD.FindkitUI({
+			publicToken: "po8GK3G0r",
+			ui: {
+				lang: "fi",
+				overrides: {
+					close: "CUSTOM",
+				},
+			},
+			params: {
+				tagQuery: [],
+			},
+		});
+
+		await ui.open();
+	});
+
+	await expect(closeButton).toHaveText("CUSTOM");
+});
+
+test("can customize ui language on the fly", async ({ page }) => {
+	await page.goto("/dummy");
+
+	const closeButton = page.locator(".findkit--close-button");
+
+	await page.evaluate(async () => {
+		const ui = new MOD.FindkitUI({
+			publicToken: "po8GK3G0r",
+			params: {
+				tagQuery: [],
+			},
+		});
+
+		await ui.open();
+		Object.assign(window, { ui });
+	});
+
+	await expect(closeButton).toHaveText("Close");
+
+	await page.evaluate(async () => {
+		ui.setUIStrings("fi", {
+			close: "CUSTOM",
+		});
+	});
+
+	await expect(closeButton).toHaveText("CUSTOM");
+});
