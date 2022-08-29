b0318e3d9f7224bb8232aa96b26d4bb54f8b7a0e
Esa-Matti Suuronen <esa-matti@suuronen.org>
2022-08-29 12:50:57 +0300

    Fix webserver command

--- packages/ui/playwright.config.ts
+++ packages/ui/playwright.config.ts
@@ -99,7 +99,7 @@ const config: PlaywrightTestConfig = {
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-		command: "npm run dev:serve",
+		command: "pnpm run dev:serve",
 		url: "http://localhost:28104/",
 		reuseExistingServer: !process.env.CI,
 	},
