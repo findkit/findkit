1c44415bfb8bdd7dab35858bedfb6932faa18aa4
Esa-Matti Suuronen <esa-matti@suuronen.org>
2022-09-02 12:26:32 +0300

    Add some visual tests

--- packages/ui/tests/visual.spec.ts
+++ packages/ui/tests/visual.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from "@playwright/test";
 
+declare const MOD: typeof import("../src/cdn-entries/index");
+
 test("input is visually correct", async ({ page }) => {
 	await page.goto("/single-group");
 
@@ -12,4 +14,44 @@ test("input is visually correct", async ({ page }) => {
 	await hits.first().waitFor({ state: "visible" });
 
 	await expect(page.locator(".findkit--header")).toHaveScreenshot();
+});
+
+test("can show backdrop", async ({ page }) => {
+	await page.goto("/dummy");
+
+	await page.evaluate(async () => {
+		const ui = new MOD.FindkitUI({
+			publicToken: "po8GK3G0r",
+			css: MOD.css`
+				.findkit--modal {
+					inset: 20px;
+				}
+			`,
+		});
+
+		await ui.open();
+	});
+	await page.locator("input").waitFor({ state: "visible" });
+	await expect(page).toHaveScreenshot();
+});
+
+test("centers the content with width css", async ({ page }) => {
+	await page.goto("/dummy");
+
+	await page.evaluate(async () => {
+		const ui = new MOD.FindkitUI({
+			publicToken: "po8GK3G0r",
+			css: MOD.css`
+				.findkit--modal {
+                    inset: initial;
+                    width: 400px;
+                    margin: 10px;
+				}
+			`,
+		});
+
+		await ui.open();
+	});
+	await page.locator("input").waitFor({ state: "visible" });
+	await expect(page).toHaveScreenshot();
 });
