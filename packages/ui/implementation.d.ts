d5fb5be31dd42315a3554b52a86f71236e9ea95b
Esa-Matti Suuronen <esa-matti@suuronen.org>
2022-09-14 14:07:59 +0300

    Fix implementation type

--- packages/ui/implementation.d.ts
+++ packages/ui/implementation.d.ts
@@ -1,6 +1,3 @@
-export {
-	Implementation,
-	implementation,
-} from "./dist/cdn-entries/implementation";
+export { Implementation, js } from "./dist/cdn-entries/implementation";
 
 export const css: string;
