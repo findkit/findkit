8016cb19f23f95b97b529d3d177622837e48a893
Esa-Matti Suuronen <esa-matti@suuronen.org>
2022-09-23 16:36:06 +0300

    Exclude e2e-visual from vitest

--- packages/ui/vitest.config.ts
+++ packages/ui/vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
-		exclude: ["**/node_modules/**", "e2e", "esm", "cjs", "dist"],
+		exclude: ["**/node_modules/**", "e2e", "e2e-visual", "esm", "cjs", "dist"],
 	},
 });
