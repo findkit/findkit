52a3f50077f85607269e9b008d68a5d30e68b35a
Esa-Matti Suuronen <esa-matti@suuronen.org>
2022-09-27 15:27:22 +0300

    example++

--- packages/ui-examples/bundled/react-embed/src/index.tsx
+++ packages/ui-examples/bundled/react-embed/src/index.tsx
@@ -1,12 +1,36 @@
 import { createRoot } from "react-dom/client";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { FindkitUI } from "@findkit/ui";
 
+const ui = new FindkitUI({
+	publicToken: "p68GxRvaA",
+});
+
 function App() {
-	return <div>hello react</div>;
+	const ref = useRef<HTMLButtonElement>(null);
+
+	useEffect(() => {
+		if (ref.current) {
+			// Return the unbind function as the effect clean up
+			return ui.openFrom(ref.current);
+		}
+	});
+
+	return (
+		<div>
+			<h1>React Embedding</h1>
+
+			<p>The fullscreen modal pattern inside React.js.</p>
+
+			<button ref={ref} type="button">
+				Open Search
+			</button>
+		</div>
+	);
 }
 
-const container = document.getElementById("app")!;
-
-const root = createRoot(container);
-root.render(<App />);
+const container = document.getElementById("app");
+if (container) {
+	const root = createRoot(container);
+	root.render(<App />);
+}
