4d62e5cd4e216876feb0f158f9de5803e4df5c34
Esa-Matti Suuronen <esa-matti@suuronen.org>
2022-09-27 15:19:26 +0300

    bundled example++

--- packages/ui-examples/bundled/react-embed/src/index.tsx
+++ packages/ui-examples/bundled/react-embed/src/index.tsx
@@ -1,1 +1,12 @@
-alert("hello");
+import { createRoot } from "react-dom/client";
+import React from "react";
+import { FindkitUI } from "@findkit/ui";
+
+function App() {
+	return <div>hello react</div>;
+}
+
+const container = document.getElementById("app")!;
+
+const root = createRoot(container);
+root.render(<App />);
