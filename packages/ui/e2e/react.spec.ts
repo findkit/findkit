387892aa927b03c8c17dff4d977ee22ab60449fa
Esa-Matti Suuronen <esa-matti@suuronen.org>
2022-09-28 11:16:50 +0300

    Add react embed test

--- packages/ui/e2e/react.spec.ts
+++ packages/ui/e2e/react.spec.ts
@@ -18,3 +18,27 @@ test("can open the fullscreen modal from react button", async ({ page }) => {
 
 	await expect(hits.first()).not.toBeVisible();
 });
+
+test("can embed inside React", async ({ page }) => {
+	await page.goto(viteEntry("embed"));
+	const hits = page.locator(".findkit--hit a");
+
+	const uiTagButton = page.locator("button", { hasText: "ui" });
+	const crawlerTagButton = page.locator("button", { hasText: "crawler" });
+
+	await expect(uiTagButton).not.toBeDisabled();
+	await expect(crawlerTagButton).toBeDisabled();
+
+	await expect(hits.first()).toBeVisible();
+
+	const initialHit = await hits
+		.first()
+		.evaluate((node: HTMLAnchorElement) => node.href);
+
+	await uiTagButton.click();
+
+	await expect(uiTagButton).toBeDisabled();
+	await expect(crawlerTagButton).not.toBeDisabled();
+
+	await expect(hits.first()).not.toHaveAttribute("href", initialHit);
+});
