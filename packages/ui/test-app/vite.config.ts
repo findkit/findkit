691fc8ff62f46993c682770d9f435d517fd85d9c
Esa-Matti Suuronen <esa-matti@suuronen.org>
2022-09-28 11:29:08 +0300

    Autoload all vite entries

--- packages/ui/test-app/vite.config.ts
+++ packages/ui/test-app/vite.config.ts
@@ -1,13 +1,30 @@
-import { resolve } from "path";
+import { resolve, join } from "path";
 import { defineConfig } from "vite";
+import { readdirSync } from "fs";
+
+const entries = readdirSync(join(__dirname, "vite"));
+
+const input = Object.fromEntries(
+	entries.flatMap((file) => {
+		if (!file.endsWith(".html")) {
+			return [];
+		}
+
+		if (file.endsWith("index.html")) {
+			return [];
+		}
+
+		const entry = [file.split(".")[0], resolve(__dirname, "vite", file)];
+
+		// Wrap to extra [] to avoid flattening
+		return [entry];
+	}),
+);
 
 export default defineConfig({
 	build: {
 		rollupOptions: {
-			input: {
-				fullscreen: resolve(__dirname, "vite/fullscreen.html"),
-				// embed: resolve(__dirname, "vite/embed.html"),
-			},
+			input,
 		},
 	},
 });
