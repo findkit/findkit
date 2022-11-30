f14a0596460cf4ef3e12f815993c29aac5b9dc99
Esa-Matti Suuronen <esa-matti@suuronen.org>
2022-11-30 10:30:05 +0200

    Test FINDKIT_GET_JWT_TOKEN global usage

--- packages/ui/e2e/dynamic.spec.ts
+++ packages/ui/e2e/dynamic.spec.ts
@@ -458,3 +458,36 @@ test("can use useTotalHitCount() in Content slot", async ({ page }) => {
 		})
 		.toBeGreaterThan(0);
 });
+
+test("calls the global FINDKIT_GET_JWT_TOKEN when defined", async ({
+	page,
+}) => {
+	await page.goto(staticEntry("/dummy"));
+
+	await page.evaluate(async () => {
+		Object.assign(window, {
+			__count: 0,
+			async FINDKIT_GET_JWT_TOKEN() {
+				this.__count++;
+			},
+		});
+
+		const ui = new MOD.FindkitUI({
+			publicToken: "po8GK3G0r",
+			minTerms: 0,
+			params: {
+				tagQuery: [],
+			},
+		});
+
+		ui.open("test");
+	});
+
+	await expect
+		.poll(async () => {
+			return await page.evaluate(async () => {
+				return (window as any).__count as number;
+			});
+		})
+		.toBeGreaterThan(0);
+});
