8652049d2b8467cafd1665d68d69810d19776796
Esa-Matti Suuronen <esa-matti@suuronen.org>
2023-10-03 11:08:40 +0300

    Add filter query support

---
+++ packages/ui/e2e/field-queries.spec.ts
@@ -0,0 +1,25 @@
+import { expect, test } from "@playwright/test";
+import { staticEntry } from "./helpers";
+
+declare const MOD: typeof import("../src/cdn-entries/index");
+
+test.only("plaa", async ({ page }) => {
+	await page.goto(staticEntry("/dummy"));
+
+	await page.evaluate(async () => {
+		const ui = new MOD.FindkitUI({
+			publicToken: "pJMDjpYOE",
+			searchEndpoint:
+				"https://staging---search.findkit.com/c/pJMDjpYOE/search?p=pJMDjpYOE",
+			minTerms: 0,
+			params: {
+				tagQuery: [],
+			},
+		});
+
+		ui.open();
+	});
+
+	const hits = page.locator(".findkit--hit");
+	await hits.first().waitFor({ state: "visible" });
+});
