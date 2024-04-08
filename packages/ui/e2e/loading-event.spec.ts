c334ccec229932fa6a3a1a4f4bdf9a80506493ac
Esa-Matti Suuronen <esa-matti@suuronen.org>
2024-04-08 10:27:50 +0300

    Cleanup loading tests
    and try to make them less flaky

--- packages/ui/e2e/loading-event.spec.ts
+++ packages/ui/e2e/loading-event.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "@playwright/test";
-import { slowDownSearch, staticEntry } from "./helpers";
+import { mockSearchResponses, slowDownSearch, staticEntry } from "./helpers";
 
 declare const MOD: typeof import("../src/cdn-entries/index");
 declare const ui: InstanceType<typeof MOD.FindkitUI>;
@@ -16,7 +16,9 @@ test("emit only one loading/loading-done event when implementation and search is
 		},
 	);
 
-	await slowDownSearch(page, 500);
+	await mockSearchResponses(page, {
+		slowDown: 500,
+	});
 
 	await page.goto(staticEntry("/dummy"));
 
@@ -71,14 +73,9 @@ test("emit only one loading/loading-done event when implementation and search is
 test("no loading event when the code and search loads fast", async ({
 	page,
 }) => {
-	await page.route(
-		(url) => url.hostname === "search.findkit.com",
-		// eslint-disable-next-line @typescript-eslint/no-misused-promises
-		async (route) => {
-			await new Promise((f) => setTimeout(f, 10));
-			await route.fulfill({ json: mockResponse });
-		},
-	);
+	await mockSearchResponses(page, {
+		// slowDown: 500,
+	});
 
 	await page.goto(staticEntry("/dummy"));
 
@@ -86,7 +83,7 @@ test("no loading event when the code and search loads fast", async ({
 		const { FindkitUI } = MOD;
 		const ui = new FindkitUI({
 			publicToken: "pW1D0p0Dg",
-			loadingThrottle: 200,
+			loadingThrottle: 400,
 		});
 
 		const uiEvents: string[] = [];
@@ -221,124 +218,3 @@ test("loading event can fire twice", async ({ page }) => {
 		})
 		.toEqual(["loading", "loading-done", "loading", "loading-done"]);
 });
-
-const mockResponse = {
-	groups: [
-		{
-			total: 3,
-			duration: 9,
-			hits: [
-				{
-					score: 32.833023,
-					superwordsMatch: false,
-					title: "Suede Boots",
-					language: "en",
-					url: "https://shop.findkit.invalid/en/clothing/suede-boots",
-					highlight:
-						"These suede <em>boots</em> provide both comfort and style. They&#x27;re crafted from high-quality suede and have a durable sole for long-lasting wear.",
-					domain: "shop.findkit.invalid",
-					tags: [
-						"category/clothing",
-						"language/en",
-						"product",
-						"domain/shop.findkit.invalid",
-					],
-					created: "2022-10-20T00:00:00.000Z",
-					modified: "2023-10-12T15:29:11.350Z",
-					customFields: {
-						category: {
-							type: "keyword",
-							value: "Clothing",
-						},
-						weight: {
-							type: "number",
-							value: 1,
-						},
-						price: {
-							type: "number",
-							value: 100,
-						},
-						quantity: {
-							type: "number",
-							value: 0,
-						},
-					},
-				},
-				{
-					score: 32.833023,
-					superwordsMatch: false,
-					title: "Leather Boots",
-					language: "en",
-					url: "https://shop.findkit.invalid/en/clothing/leather-boots",
-					highlight:
-						"Western-inspired <em>boots</em> crafted in chrome-free leather with pointed toes and angled heels. Featuring soft shafts made from stretch nappa, finished with concealed zippers on the inside.",
-					domain: "shop.findkit.invalid",
-					tags: [
-						"category/clothing",
-						"language/en",
-						"product",
-						"domain/shop.findkit.invalid",
-					],
-					created: "2022-10-26T00:00:00.000Z",
-					modified: "2023-10-12T15:29:11.689Z",
-					customFields: {
-						category: {
-							type: "keyword",
-							value: "Clothing",
-						},
-						weight: {
-							type: "number",
-							value: 1,
-						},
-						price: {
-							type: "number",
-							value: 220,
-						},
-						quantity: {
-							type: "number",
-							value: 4,
-						},
-					},
-				},
-				{
-					score: 32.833023,
-					superwordsMatch: false,
-					title: "Rubber Boots",
-					language: "en",
-					url: "https://shop.findkit.invalid/en/clothing/rubber-boots",
-					highlight:
-						"Rubber <em>boots</em> which will keep your feet dry even int the heaviest rain",
-					domain: "shop.findkit.invalid",
-					tags: [
-						"category/clothing",
-						"language/en",
-						"product",
-						"domain/shop.findkit.invalid",
-					],
-					created: "2022-10-20T00:00:00.000Z",
-					modified: "2023-10-12T15:29:11.818Z",
-					customFields: {
-						category: {
-							type: "keyword",
-							value: "Clothing",
-						},
-						weight: {
-							type: "number",
-							value: 1,
-						},
-						price: {
-							type: "number",
-							value: 30,
-						},
-						quantity: {
-							type: "number",
-							value: 34,
-						},
-					},
-				},
-			],
-		},
-	],
-	duration: 31,
-	messages: [],
-};
